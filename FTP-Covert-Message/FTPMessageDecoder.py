45ce4914b4567748459c02b7250cd1e0e7521142
Eric Pitts <epitts1013@gmail.com>
2020-09-30 14:40:19 -0500

    Create FTPMessageDecoder.py

---
+++ FTP-Covert-Message/FTPMessageDecoder.py
@@ -0,0 +1,27 @@
+from ftplib import FTP
+
+# FTP server details
+IP = "138.47.99.5"
+PORT = 21
+USER = "anonymous"
+PASSWORD = ""
+FOLDER = "/7/"
+USE_PASSIVE = True # set to False if the connection times out
+
+# connect and login to the FTP server
+ftp = FTP()
+ftp.connect(IP, PORT)
+ftp.login(USER, PASSWORD)
+ftp.set_pasv(USE_PASSIVE)
+
+# navigate to the specified directory and list files
+ftp.cwd(FOLDER)
+files = []
+ftp.dir(files.append)
+
+# exit the FTP server
+ftp.quit()
+
+# display the folder contents
+for f in files:
+	print(f)
