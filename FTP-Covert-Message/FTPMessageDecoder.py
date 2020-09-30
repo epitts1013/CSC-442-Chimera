b5bdd586a7c51d5a4f4ac547d57dd41fabf75aec
Eric Pitts <epitts1013@gmail.com>
2020-09-30 15:11:47 -0500

    Update FTPMessageDecoder.py

--- FTP-Covert-Message/FTPMessageDecoder.py
+++ FTP-Covert-Message/FTPMessageDecoder.py
@@ -1,5 +1,35 @@
 from ftplib import FTP
 
+### METHODS ###
+def formatFileList(fileList, sevenBit):
+    formattedFileList = []
+
+    # if using seven rightmost bits of file permissions
+    if (sevenBit):
+        # loop through file list to extract file permissions from each string in fileList
+        for file in fileList:
+            if (file[:3] == "---"):
+                formattedFileList.append(file[3:10])
+    else:
+        pass
+
+    return formattedFileList
+
+# decodes message encoded in file permissions
+def convertToBitString(filePermissions):
+    textString = "".join(filePermissions)
+    bits = []
+
+    for char in textString:
+        if (char == "-"):
+            bits.append("0")
+        else:
+            bits.append("1")
+
+    return "".join(bits)
+
+### MAIN ###
+### FTP Connect Code from Dr. Timofeyev ###
 # FTP server details
 IP = "138.47.99.5"
 PORT = 21
@@ -22,6 +52,12 @@ ftp.dir(files.append)
 # exit the FTP server
 ftp.quit()
 
-# display the folder contents
-for f in files:
-	print(f)
+# for file in files:
+#     print(file)
+
+# set code to be executed based on whether we are looking in the /7/ directory
+if (FOLDER == "/7/"):
+    bitString = convertToBitString(formatFileList(files, True))
+    print(bitString)
+elif (FOLDER == "/10/"):
+    pass
