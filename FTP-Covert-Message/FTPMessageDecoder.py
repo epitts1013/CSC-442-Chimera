4ccaf9b7d3dbd448c787f62548f081e13334f900
AlexanderLFloyd <32877587+AlexanderLFloyd@users.noreply.github.com>
2020-09-30 15:18:22 -0500

    Added 10 bit decoder

--- FTP-Covert-Message/FTPMessageDecoder.py
+++ FTP-Covert-Message/FTPMessageDecoder.py
@@ -25,3 +25,21 @@ ftp.quit()
 # display the folder contents
 for f in files:
 	print(f)
+
+
+def tenBitDecoder(files):
+	permissions = ""
+	for f in files:
+	        file_permissions = f[0:10]
+	        bin_rep = ""
+	        for i in file_permissions:
+	                if(i == "-"):
+	                        bin_rep += "0"
+	                else:
+	                        bin_rep += "1"
+	        permissions += bin_rep
+
+	msg = ""
+	for i in range(0,len(permissions),7):
+	        msg += chr(int(permissions[i:i+7],2))
+	print(msg)
