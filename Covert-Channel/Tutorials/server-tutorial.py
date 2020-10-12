002d4f102c881c8667958527205027b03d094a2b
Eric Pitts <epitts1013@gmail.com>
2020-10-12 14:35:57 -0500

    Create Covert-Channel folder, upload tutorials and assignment PDF

---
+++ Covert-Channel/Tutorials/server-tutorial.py
@@ -0,0 +1,30 @@
+import socket
+from time import sleep
+
+# set the port for client connections
+port = 1337
+
+# create the socket and bind it to the port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", port))
+
+# listen for clients
+# this is a blocking call
+s.listen(0)
+
+# a client has connected!
+c, addr = s.accept()
+
+# set the message
+msg = "Some message...\n"
+
+# send the message, one letter at a time
+for i in msg:
+	c.send(i)
+	# delay a bit in between each letter
+	sleep(0.1)
+
+# send EOF and close the connection to the client
+c.send("EOF")
+c.close()
+
