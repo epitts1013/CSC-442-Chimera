310077bbeb6c3a79e7c2819341b154b96771ced3
epitts1013 <epitts1013@gmail.com>
2020-10-14 11:54:09 -0500

    Worked on CovertMessage.py, still no solution

--- Covert-Channel/CovertChannel.py
+++ Covert-Channel/CovertChannel.py
@@ -1,13 +1,28 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from sys import stdout
 from time import time
+from textwrap import wrap
+
+def binaryConvert(content, length):
+    text = ""
+
+    wrapped_content = wrap(content, length)
+
+    for character in wrapped_content:
+
+        int_content = int(character, 2)
+        text += chr(int_content)
+
+    print("=============================================")
+    print("Solution : " + text)
+    print("=============================================")
 
 # server information variables
 serverIP = "localhost"
 portNum = 1337
 
 # set time threshold for "short" and "long" delay
-delayThreshold = 0.25
+delayThreshold = 0.1
 
 # create socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -19,7 +34,7 @@ clientSocket.connect((serverIP, portNum))
 data = clientSocket.recv(4096)
 
 # continue receiving data until recieving an EOF message
-binaryMessage = ""
+timeDeltas = []
 while (data.decode().rstrip("\n") != "EOF"):
     # output data to stdout
     stdout.write(data.decode())
@@ -29,14 +44,23 @@ while (data.decode().rstrip("\n") != "EOF"):
     startTime = time()
     data = clientSocket.recv(4096)
     endTime = time()
+    timeDeltas.append(endTime-startTime)
 
+
+# for delta in timeDeltas:
+#     print(round(delta, 3))
+
+binaryMessage = ""
+for delta in timeDeltas:
     # calculate time between messages
-    timeDelta = round(endTime - startTime, 3)
+    delta = round(delta, 3)
 
     # compare timeDelta to delayThreshold
-    if (timeDelta < delayThreshold):
+    if (delta < delayThreshold):
         binaryMessage += "0"
     else:
         binaryMessage += "1"
 
 print(binaryMessage)
+
+binaryConvert(binaryMessage, 7)
---
+++ Covert-Channel/CovertChannelTestServer.py
@@ -0,0 +1,40 @@
+import socket
+from time import sleep
+from random import randint
+
+# set the port for client connections
+port = 1337
+
+# create the socket and bind it to the port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", port))
+
+# listen for clients
+# this is a blocking call
+s.listen(0)
+
+# a client has connected!
+c, addr = s.accept()
+
+# set the message
+msg = "Some message...\n"
+
+covertMessage = "Testing..."
+binaryMessage = ""
+for char in covertMessage:
+    binaryMessage += bin(ord(char))[2:]
+
+print(binaryMessage)
+
+# send the message, one letter at a time
+for i in range(len(binaryMessage)):
+	c.send(msg[i%len(msg)].encode())
+	# delay a bit in between each letter
+	if (binaryMessage[i] == "0"):
+		sleep(0.025)
+	else:
+		sleep(0.1)
+
+# send EOF and close the connection to the client
+c.send("EOF".encode())
+c.close()
