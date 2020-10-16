97c670de860c063d66bd32e53ced1799d8b816bf
AlexanderLFloyd <32877587+AlexanderLFloyd@users.noreply.github.com>
2020-10-16 12:01:55 -0500

    Update CovertChannel.py

--- Covert-Channel/CovertChannel.py
+++ Covert-Channel/CovertChannel.py
@@ -1,102 +1,102 @@
-import socket, os
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM
 from sys import stdout
 from time import time
-import sys
 from textwrap import wrap
-import matplotlib.pyplot as pp
+
+DEBUG_MODE = True
 
 
-# Plots deltas into a 1-D scatter plots for visible differentiation
-def d_plot(deltas):
-	pp.plot(deltas, len(deltas)*[1], ".")
-	pp.show()
+def decode(text):
+    deltTot = 0
+    binaryMessage = ""
+    for delta in timeDeltas[10:-10]:
+        deltTot += delta
+        # calculate time between messages
+        delta = round(delta, 3)
 
-# Decodes delta array into a bitstring
-def decode(delta):
-	mp = get_midpoint(deltas)
-	decoded_text = ""
-	for c in deltas:
-		decoded_text += separate(mp,c)
-	return decoded_text
+        # compare timeDelta to delayThreshold
+        #if (delta < delayThreshold):
+        #    binaryMessage += "0"
+        #else:
+        #    binaryMessage += "1"
+    deltAvg = deltTot/len(timeDeltas[10:-10])
+    for delta in timeDeltas:
+        if (delta < deltAvg):
+            binaryMessage += "0"
+        else:
+            binaryMessage += "1"
+    print(binaryMessage)
 
-# Returns a 1 or 0, determined by if the passed delta is above or below midpoint
-def separate(midpoint,d):
-	HIGH = 0.052
-	LOW = 0.038
-	#if(abs(d-LOW) < abs(d-HIGH)):
-	#	return "0"
-	#else:
-	#	return "1"
-	if(d < midpoint):
-		return '0'
-	else:
-		return '1'
+    binaryConvert(binaryMessage, 8)
 
-# Converts a bitstring to a utf string
-def binary_convert(content, length = 7):
+
+# convert binary string to equivalent ascii text
+def binaryConvert(content, length):
     text = ""
-
-    #if (len(content) % length) == 0:
-    content = content[:(len(content) - (len(content) % length))]
 
     wrapped_content = wrap(content, length)
 
     for character in wrapped_content:
+
         int_content = int(character, 2)
         text += chr(int_content)
 
-    return (text)
-
-# Calculates the midpoint for the set of deltas
-def get_midpoint(deltas):
-	total = 0.0
-	for delta in deltas:
-		total += delta
-	return total/len(deltas)
-
+    print("Solution : " + text)
 
 # server information variables
-ip = "138.47.98.190"
-port = 31337
+serverIP = "138.47.99.29"
+portNum = 33333
+
+# set time threshold for "short" and "long" delay
+#delayThreshold = 0.1
 
 # create socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 # establish connection to server
-clientSocket.connect((ip, port))
+clientSocket.connect((serverIP, portNum))
 
 # receive initial data packet from server
 data = clientSocket.recv(4096)
 
-# Set of delta values, containing the delay for each character recieved
-deltas = []
+# continue receiving data until recieving an EOF message
+timeDeltas = []
+while (data.decode().rstrip("\n") != "EOF"):
+    # output data to stdout
+    stdout.write(data.decode())
+    stdout.flush()
 
-# receive data until EOF
-data = clientSocket.recv(4096).decode('UTF-8')
-stdout.write(data)
-while (data.rstrip("\n") != "EOF"):
-	# output the data
-	stdout.write(data)
-	stdout.flush()
-	# start the "timer", get more data, and end the "timer"
-	t0 = time()
-	data = clientSocket.recv(4096).decode('UTF-8')
-	t1 = time()
-	# calculate the time delta (and output if debugging)
-	delta = round(t1 - t0, 3)
-	#stdout.write(" {}\n".format(delta))
-        # Subtracts ping to server from the current delta
-	deltas.append(delta)#-os.system("ping -c 1 138.47.98.190")
-	stdout.flush()
+    # get time until next message recieved
+    startTime = time()
+    data = clientSocket.recv(4096)
+    endTime = time()
+    timeDeltas.append(endTime - startTime)
+    if(len(timeDeltas)%700 == 0):
+        decode(timeDeltas)
 
-# close the connection to the server
-clientSocket.close()
-# Plots the delta values from all recieved characters into a 1-D scatter plot
-d_plot(deltas)
-print("Midpoint: ",get_midpoint(deltas))
-# Decodes the delta values into a bitstring, then into a utf string
-print(binary_convert(decode(deltas)))
+#if (DEBUG_MODE):
+#    orderedDeltas = timeDeltas.copy()
+#    orderedDeltas.sort()
+#    for delta in orderedDeltas:
+deltTot = 0
+binaryMessage = ""
+for delta in timeDeltas[10:-10]:
+    deltTot += delta
+    # calculate time between messages
+    delta = round(delta, 3)
 
-#deltas.sort()
-#print(deltas)
+    # compare timeDelta to delayThreshold
+    #if (delta < delayThreshold):
+    #    binaryMessage += "0"
+    #else:
+    #    binaryMessage += "1"
+deltAvg = deltTot/len(timeDeltas[10:-10])
+for delta in timeDeltas:
+    if (delta < deltAvg):
+        binaryMessage += "0"
+    else:
+        binaryMessage += "1"
+
+print(binaryMessage)
+
+binaryConvert(binaryMessage, 8)
