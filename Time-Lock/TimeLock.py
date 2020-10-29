d112cb84b4f0f43ba512021ecbf0915260f68511
Eric Pitts <epitts1013@gmail.com>
2020-10-29 17:54:27 -0500

    5:54

--- Time-Lock/TimeLock.py
+++ Time-Lock/TimeLock.py
@@ -21,19 +21,15 @@ epochTime = time.mktime(time.strptime(stdinEpoch, "%Y %m %d %H %M %S"))
 
 # debug mode allows manual time entry
 if (DEBUG):
-    stdinEpoch = "1999 12 31 23 59 59"
-    currentTime = "2017 04 23 18 02 30"
+    stdinEpoch = "1974 06 01 08 57 23"
+    currentTime = "2017 04 26 15 14 30"
     currentTime = time.mktime(time.strptime(currentTime, "%Y %m %d %H %M %S"))
-    print(currentTime)
     currentTime = currentTime - (currentTime % 60)
-    print(currentTime)
     epochTime = time.mktime(time.strptime(stdinEpoch, "%Y %m %d %H %M %S"))
     print(epochTime)
 
 # get time elapsed since given epoch
-elapsedTime = currentTime - epochTime
-
-print(floor(elapsedTime))
+elapsedTime = floor(currentTime - epochTime)
 
 # compute MD5 hash
 hashedTime = md5(str(elapsedTime).encode())
